ee7a4990a2d30f9032b7380243f8b78ec894e305
beikehanbao23 <369103724@qq.com>
2018-01-31 10:04:45 +0800

    update db script

--- data/scripts/db.sql
+++ data/scripts/db.sql
@@ -1,50 +1,66 @@
+create database coxosm template template_postgis;
+create extension hstore;
+
 -- ----------------------------
---  Table structure for map_places
+--  Table structure for place
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."place";
 CREATE TABLE "public"."place" (
-	"id" int8 NOT NULL DEFAULT nextval('id_seq'::regclass),
-	"arcgisid" int4 NOT NULL DEFAULT nextval('arcgisid_seq'::regclass),
-	"osmid" int8,
-	"sourceid" varchar COLLATE "default",
-	"render_nm" varchar COLLATE "default",
-	"render_nm_en" varchar COLLATE "default",
-    "render_nm_cn" varchar COLLATE "default",
-	"render_nm_tw" varchar COLLATE "default",
-    "render_nm_hk" varchar COLLATE "default",
-    "render_nm_in" varchar COLLATE "default",
-    "render_icon" varchar DEFAULT 'default'::character varying COLLATE "default",
-    "render_from" int2,
-	"render_to" int2,
-	"search_nm" varchar(255) COLLATE "default",
-	"search_nm_cn" varchar(255) COLLATE "default",
-	"search_nm_en" varchar(255) COLLATE "default",
-	"search_nm_tw" varchar(255) COLLATE "default",
-    "search_nm_hk" varchar(255) COLLATE "default",
-    "search_nm_id" varchar(255) COLLATE "default",
-	"zone" varchar COLLATE "default",
-    "zone_nm" varchar COLLATE "default",
-	"population" int4,
-	"capital" bool,
-	"is_capital" bool,
-	"admin_level" int4,
+    "id" int8  PRIMARY KEY,
+    "arcgisid" int4,
+    "osmid" int8,
+    "pid" int8,
+    "sourceid" varchar COLLATE "default",
+    "name" varchar(255) COLLATE "default",
+    "name_cn" varchar(255) COLLATE "default",
+    "name_en" varchar(255) COLLATE "default",
+    "name_other" varchar(255) COLLATE "default",
+    "zone" varchar(255) COLLATE "default",
+    "zone_name" varchar(255) COLLATE "default",
+    "seo_title_en" varchar(255) COLLATE "default",
+    "seo_desc_en" text COLLATE "default",
+    "seo_title_cn" varchar(255) COLLATE "default",
+    "seo_desc_cn" text COLLATE "default",
+    "seo_title_other" varchar(255) COLLATE "default",
+    "seo_desc_other" text COLLATE "default",
+    "desc_en" text COLLATE "default",
+    "desc_cn" text COLLATE "default",
+    "desc_other" text COLLATE "default",
+    "population" int4,
+    "capital" bool,
+    "is_capital" bool,
+    "admin_level" int4,
     "admin_type" int4,
-	"scalerank" int4,
-	"geometry" "public"."geometry",
-    "geometry_area" "public"."geometry",
-	"is_rendered" bool,
-	"is_searched" bool,
+    "geometry" "public"."geometry",
     "is_delete" bool,
-	"weight" int8,
-	"timestamp" timestamp(6) NULL,
-    "update_time" timestamp(6) NULL,
+    "weight" int8,
+    "timestamp" timestamp(6) NULL,
+    "update_timestamp" timestamp(6) NULL 
 )
 WITH (OIDS=FALSE);
-ALTER TABLE "public"."vector_gullmap_places" OWNER TO "postgres";
+ALTER TABLE "public"."place" OWNER TO "postgres";
+
+CREATE SEQUENCE global_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+CREATE SEQUENCE arcgis_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;    
+  
+alter table place alter column id set default nextval('global_id_seq');
+alter table place alter column arcgisid set default nextval('arcgis_id_seq');
 
 -- ----------------------------
 --  Indexes structure for table places
 -- ----------------------------
-CREATE INDEX  "place_idx" ON "public"."vector_gullmap_places" USING btree(id ASC NULLS LAST);
-CREATE INDEX  "place_geom_idx" ON "public"."vector_gullmap_places" USING gist(geometry);
-CREATE INDEX  "place_geom_area_idx" ON "public"."vector_gullmap_places" USING gist(geometry_area);
+CREATE INDEX  "place_idx" ON "public"."place" USING btree(id ASC NULLS LAST);
+CREATE INDEX  "place_geom_idx" ON "public"."place" USING gist(geometry);
+
+CREATE INDEX ON ON "public"."place"
+USING btree (st_geohash(st_transform(st_setsrid(box2d(geometry)::geometry, 3857), 4326)));
